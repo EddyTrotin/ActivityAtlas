88b96f8291ccdaefd0dbb24006b1fea7130f23ae
EddyTrotin <dev.eddy.trotin@outlook.com>
2024-02-16 18:46:36 +0100

    add few tests

--- components/ActivityList.vue
+++ components/ActivityList.vue
@@ -5,20 +5,18 @@ const activities = useActivityStore()
 </script>
 
 <template>
-  <div class="h-full px-6 pb-10">
-    <div class="h-full overflow-y-scroll flex flex-wrap justify-center">
-      <div
-        v-for="activity in activities.list"
-        @mouseover="activities.current = activity.id"
-        class="card w-96 h-96 shadow-xl m-4"
-        :class="activities.current === activity.id ? 'bg-[#FF6455]' : 'bg-gray-200'"
-      >
-        <div class="card-body">
-          <h2 class="card-title">{{ activity.title }}</h2>
-          <p>{{ activity.desc }}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
+  <div class="h-full mx-6 pb-8 overflow-y-scroll flex flex-wrap justify-center">
+    <div
+      v-for="activity in activities.list"
+      @mouseover="activities.current = activity.id"
+      class="card w-96 h-96 shadow-xl m-4"
+      :class="activities.current === activity.id ? 'bg-[#FF6455]' : 'bg-gray-200'"
+    >
+      <div class="card-body">
+        <h2 class="card-title">{{ activity.title }}</h2>
+        <p>{{ activity.desc }}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
--- tests/stores/activity.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-
-import { useActivityStore } from '../../stores/activity'
-
-test('my test', () => {
-  const store = useActivityStore()
-  expect(store.list).toHaveLength(20)
-  expect(store.current).toBe('')
-})
